5d104bc8a775693a156c69ddad970a4e96c75651
gdsrosa <gabrieldsrosa@gmail.com>
2019-04-14 18:52:54 -0300

    (*) Add new rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,14 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'class-methods-use-this': [0, { exceptMethods: '' }],
+    'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': [0, { allowShortCircuit: true, allowTernary: true }],
+    'no-plusplus': [0],
+    'react/forbid-prop-types': [
+      0,
+      { forbid: [''], checkContextTypes: false, checkChildContextTypes: false },
+    ],
   },
 };
