a1b3f0f127f627ba5d7cf5f2156c28417b42f39d
gdsrosa <gabrieldsrosa@gmail.com>
2019-04-15 00:52:29 -0300

    (*) Modify label and refactor Question component

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,8 +4,8 @@ const Instructions = () => (
   <div>
     <h1>Teste seus conhecimentos sobre a saga Harry Potter</h1>
     <p>
-      Neste quiz você irá responder 6 questões sobre o famoso bruxo cujo seus pais foram mortos por
-      <em>Você sabe quem.</em>
+      Neste quiz você irá responder 5 questões sobre o famoso bruxo cujo seus pais foram mortos por
+      <em> Você sabe quem.</em>
     </p>
   </div>
 );
